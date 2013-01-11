d432de08232f74bc9dd6dd67c27b3b50f009e630
Lukas Vlcek <lukas.vlcek@gmail.com>
2013-01-11 13:36:24 +0100

    soy template for individual search result hit

--- src/main/javascript_source/generated_templates/search_results.soy.js
+++ src/main/javascript_source/generated_templates/search_results.soy.js
@@ -20,8 +20,22 @@ org.jboss.search.page.templates.search_results = function(opt_data, opt_sb) {
   var hitListLen8 = hitList8.length;
   for (var hitIndex8 = 0; hitIndex8 < hitListLen8; hitIndex8++) {
     var hitData8 = hitList8[hitIndex8];
-    output.append('<div class="hit"><div class="left"><p class="avatar"><img src="image/test/sanne.png"></p></div><div class="main"><div class="title">Title</div><div class="link"><a href="#">#</a></div><div class="snippet"><span class="date">DD/MMM/YYYY - </span>Snippet&nbsp;&hellip;</div></div></div><div class="hit_spacer"></div>');
+    org.jboss.search.page.templates.hit(hitData8, output);
+    output.append('<div class="hit_spacer"></div>');
   }
-  output.append('<div class="pagination">&#9668;<span>3</span><span>4</span><span>5</span><span>6</span><span>7</span><span>8</span><span>9</span><span>10</span><span>11</span>&#9654;</div>');
+  output.append('<div class="pagination"><span> &#9668;</span><span> 3</span><span> 4</span><span> 5</span><span> 6</span><span> 7</span><span> 8</span><span> 9</span><span> 10</span><span> 11</span><span> &#9654;</span></div>');
+  return opt_sb ? '' : output.toString();
+};
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {soy.StringBuilder=} opt_sb
+ * @return {string}
+ * @notypecheck
+ */
+org.jboss.search.page.templates.hit = function(opt_data, opt_sb) {
+  var output = opt_sb || new soy.StringBuilder();
+  output.append('<div class="hit"><div class="left"><p class="avatar"><img src="image/test/sanne.png"></p></div><div class="main"><div class="title">Title</div><div class="link"><a href="#">#</a></div><div class="snippet"><span class="date">DD/MMM/YYYY - </span>Snippet&nbsp;&hellip;</div></div></div>');
   return opt_sb ? '' : output.toString();
 };
--- src/main/javascript_source/generated_templates/suggestion.soy.js
+++ src/main/javascript_source/generated_templates/suggestion.soy.js
@@ -16,11 +16,11 @@ goog.require('soy.StringBuilder');
 org.jboss.search.suggestions.templates.suggestions_section = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
   output.append('<div class="suggestions_section">');
-  var optionList15 = opt_data.options;
-  var optionListLen15 = optionList15.length;
-  for (var optionIndex15 = 0; optionIndex15 < optionListLen15; optionIndex15++) {
-    var optionData15 = optionList15[optionIndex15];
-    output.append('<div class="selectable" index="', soy.$$escapeHtml(opt_data.indexStart + optionIndex15), '">', (optionIndex15 == 0) ? '<div class="caption">' + soy.$$escapeHtml(opt_data.caption) + '</div>' : '', '<div class="option">', optionData15, '</div></div>');
+  var optionList18 = opt_data.options;
+  var optionListLen18 = optionList18.length;
+  for (var optionIndex18 = 0; optionIndex18 < optionListLen18; optionIndex18++) {
+    var optionData18 = optionList18[optionIndex18];
+    output.append('<div class="selectable" index="', soy.$$escapeHtml(opt_data.indexStart + optionIndex18), '">', (optionIndex18 == 0) ? '<div class="caption">' + soy.$$escapeHtml(opt_data.caption) + '</div>' : '', '<div class="option">', optionData18, '</div></div>');
   }
   output.append('<div class="suggestion_section_last"></div></div>');
   return opt_sb ? '' : output.toString();
