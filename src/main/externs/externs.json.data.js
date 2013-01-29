b3fae560211de87c117679f00e904de6ac745561
Lukas Vlcek <lukas.vlcek@gmail.com>
2013-01-29 15:02:26 +0100

    * added externs for JSON data pulled from search REST API * reverted changed to test data

---
+++ src/main/externs/externs.json.data.js
@@ -0,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Externs for json data structures that are input to Soy templates.
+ * @author Lukas Vlcek (lvlcek@redhat.com)
+ * @externs
+ */
+
+// The purpose of this externs file is to make sure the compiler does not rename JSON fields.
+// see https://groups.google.com/forum/?fromgroups=#!topic/closure-templates-discuss/dRgUIbp84iw
+
+/**
+ * Search hits.
+ * @interface */
+function SearchResults() {};
+SearchResults.prototype.user_query;
+SearchResults.prototype.hits;
+SearchResults.prototype.hits.total;
+SearchResults.prototype.hits.hits;
+
+/**
+ * Individual search hit.
+ * @interface */
+function SearchHit() {};
+SearchHit.prototype.fields;
+SearchHit.prototype.fields.dcp_contributors;
+SearchHit.prototype.fields.dcp_title;
+SearchHit.prototype.fields.dcp_url_view;
+SearchHit.prototype.fields.dcp_last_activity_date;
+SearchHit.prototype.fields.dcp_description;
+// added by normalize.js
+SearchHit.prototype.fields.dcp_url_view_tr;
+SearchHit.prototype.fields.contributor_gravatar;
+SearchHit.prototype.fields.dcp_description_tr;
