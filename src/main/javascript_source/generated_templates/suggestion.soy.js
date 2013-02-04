e9fb5fbb9c7daf9d0d5e1f8ba3dfbff7f0f05a70
Lukas Vlcek <lukas.vlcek@gmail.com>
2013-02-04 16:43:23 +0100

    make search hit 'title' clickable

--- src/main/javascript_source/generated_templates/suggestion.soy.js
+++ src/main/javascript_source/generated_templates/suggestion.soy.js
@@ -15,11 +15,11 @@ goog.require('soydata');
  */
 org.jboss.search.suggestions.templates.suggestions_section = function(opt_data, opt_ignored) {
   var output = '<div class="suggestions_section">';
-  var optionList30 = opt_data.options;
-  var optionListLen30 = optionList30.length;
-  for (var optionIndex30 = 0; optionIndex30 < optionListLen30; optionIndex30++) {
-    var optionData30 = optionList30[optionIndex30];
-    output += '<div class="selectable" index="' + soy.$$escapeHtml(opt_data.indexStart + optionIndex30) + '">' + ((optionIndex30 == 0) ? '<div class="caption">' + soy.$$escapeHtml(opt_data.caption) + '</div>' : '') + '<div class="option">' + soy.$$filterNoAutoescape(optionData30) + '</div></div>';
+  var optionList32 = opt_data.options;
+  var optionListLen32 = optionList32.length;
+  for (var optionIndex32 = 0; optionIndex32 < optionListLen32; optionIndex32++) {
+    var optionData32 = optionList32[optionIndex32];
+    output += '<div class="selectable" index="' + soy.$$escapeHtml(opt_data.indexStart + optionIndex32) + '">' + ((optionIndex32 == 0) ? '<div class="caption">' + soy.$$escapeHtml(opt_data.caption) + '</div>' : '') + '<div class="option">' + soy.$$filterNoAutoescape(optionData32) + '</div></div>';
   }
   output += '<div class="suggestion_section_last"></div></div>';
   return output;
