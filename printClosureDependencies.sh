61b93f196faecc933d4b55400e51e4308def487f
Lukas Vlcek <lukas.vlcek@gmail.com>
2013-03-11 17:54:53 +0100

    fixing dependencies for jsTestDriver tests

--- printClosureDependencies.sh
+++ printClosureDependencies.sh
@@ -18,6 +18,8 @@
   --namespace="org.jboss.search.Constants" \
   --namespace="org.jboss.search.SearchFieldHandler" \
   \
+  --namespace="org.jboss.search.logging.Logging" \
+  \
   --namespace="org.jboss.search.suggestions.event.EventType" \
   --namespace="org.jboss.search.suggestions.event.SearchStart" \
   --namespace="org.jboss.search.suggestions.event.SearchFinish" \
